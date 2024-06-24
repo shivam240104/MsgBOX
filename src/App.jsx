3901257f5d83e87340af7e7a59c0187e21cf1bb3
shivam240104 <mauryatanmay845@gmail.com>
2024-06-24 07:26:28 +0530

    first react vite activity

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Msg from './msg'
+
+function App() {
+  
+
+  return (
+    <>
+    <Msg userName={"world"} textColor={"red"}/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/msg.jsx
@@ -0,0 +1,12 @@
+function msg({userName, textColor}){
+
+    
+    return ( 
+        <>
+       
+    <h1 style={{color:textColor}} >hello {userName} ,by Devil!</h1>
+    </>
+)
+}
+
+export default msg;
